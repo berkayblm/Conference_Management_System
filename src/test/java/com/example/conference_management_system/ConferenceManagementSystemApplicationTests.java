c1557956c98f702000103153a5dd3d7269c14927
Berkay Bilimli <berkay-bilimli@hotmail.com>
2024-04-30 15:31:39 +0300

    a

---
+++ src/test/java/com/example/conference_management_system/ConferenceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.conference_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

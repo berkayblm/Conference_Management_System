841693b38ff63d1a9f491bb31fcb804173295137
Ramazan Ozer <ozerramazan991@gmail.com>
2024-05-06 10:08:37 +0300

    update nav bar depends on the user role feature added

---
+++ src/main/resources/static/main/index.js
@@ -0,0 +1,18 @@
+const itemPaper = document.getElementById("papers");
+const itemSubmittedReviews = document.getElementById("submitted_reviews");
+
+let userRole = sessionStorage.getItem("userRole");
+if(userRole === "Reviewer"){
+    console.log("reviwer");
+    // itemSubmittedReviews.style.display = "none";
+    itemSubmittedReviews.style.display = "block"; // To show the item
+    itemPaper.style.display = "none";
+
+} else if(userRole === "Author"){
+    console.log("author");
+
+    itemSubmittedReviews.style.display = "none"; // To show the item
+    itemPaper.style.display = "block";
+} else {
+    console.log(userRole);
+}

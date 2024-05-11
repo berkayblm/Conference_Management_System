9bc01703967f626d24535d764359d743ab63b69a
Ramazan Ozer <ozerramazan991@gmail.com>
2024-05-11 21:57:35 +0300

    update ui

--- src/main/resources/static/main/changeVisibility.js
+++ src/main/resources/static/main/changeVisibility.js
@@ -2,40 +2,59 @@ const itemPaper = document.getElementById("papers");
 const itemSubmittedReviews = document.getElementById("submitted_reviews");
 const itemConferenceSetup = document.getElementById("conferenceSetup")
 let userRole = sessionStorage.getItem("userRole");
+
 if(userRole === "Reviewer"){
     console.log("reviwer");
     // itemSubmittedReviews.style.display = "none";
     itemSubmittedReviews.style.display = "block"; // To show the item
     itemPaper.style.display = "none";
-    itemConferenceSetup.style.display = "none";
+    try{
+        itemConferenceSetup.style.display = "none";
+    } catch (Exception){
+
+    }
 
 } else if(userRole === "Author"){
     console.log("author");
 
     itemSubmittedReviews.style.display = "none"; // To show the item
     itemPaper.style.display = "block";
-    itemConferenceSetup.style.display = "none";
+    try{
+        itemConferenceSetup.style.display = "none";
+    } catch (Exception){
+
+    }
 } else if(userRole === "Organizer"){
     console.log("organizer");
     itemPaper.style.display = "none";
-    itemConferenceSetup.style.display = "block";
+    try{
+        itemConferenceSetup.style.display = "none";
+    } catch (Exception){
+
+    }
     itemSubmittedReviews.style.display = "none";
 } else {
     console.log("asdsa")
     itemSubmittedReviews.style.display = "none";
     itemPaper.style.display = "none";
-    itemConferenceSetup.style.display = "none";
+    try{
+        itemConferenceSetup.style.display = "none";
+    } catch (Exception){
+
+    }
     console.log(userRole);
 }
 
-
-document.addEventListener("DOMContentLoaded", function() {
-    updateNavbar();
-});
+updateNavbar();
+// document.addEventListener("DOMContentLoaded", function() {
+//     console.log("it is being called v1")
+//     updateNavbar();
+// });
 
 function updateNavbar() {
     const userName = sessionStorage.getItem("userName");
     const userRole = sessionStorage.getItem("userRole");
+    console.log("it is being called")
     if (userName && userRole) {
         document.getElementById("userNameDisplay").textContent = userName;
         document.getElementById("userRoleDisplay").textContent = userRole;

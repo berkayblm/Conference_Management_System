dffc6f82157fcbd093c9a9b3eb1342f161389bfa
Arda <ardasecureal@gmail.com>
2024-05-11 16:48:45 +0300

    conference setup visibility set

--- src/main/resources/static/main/changeVisibility.js
+++ src/main/resources/static/main/changeVisibility.js
@@ -1,22 +1,30 @@
 const itemPaper = document.getElementById("papers");
 const itemSubmittedReviews = document.getElementById("submitted_reviews");
-
+const itemConferenceSetup = document.getElementById("conferenceSetup")
 let userRole = sessionStorage.getItem("userRole");
 if(userRole === "Reviewer"){
     console.log("reviwer");
     // itemSubmittedReviews.style.display = "none";
     itemSubmittedReviews.style.display = "block"; // To show the item
     itemPaper.style.display = "none";
+    itemConferenceSetup.style.display = "none";
 
 } else if(userRole === "Author"){
     console.log("author");
 
     itemSubmittedReviews.style.display = "none"; // To show the item
     itemPaper.style.display = "block";
+    itemConferenceSetup.style.display = "none";
+} else if(userRole === "Organizer"){
+    console.log("organizer");
+    itemPaper.style.display = "none";
+    itemConferenceSetup.style.display = "block";
+    itemSubmittedReviews.style.display = "none";
 } else {
     console.log("asdsa")
     itemSubmittedReviews.style.display = "none";
     itemPaper.style.display = "none";
+    itemConferenceSetup.style.display = "none";
     console.log(userRole);
 }
 
